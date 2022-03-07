61a6e26dc0f4ebbbe614cd20b34832062facf630
Auxione <crouzer97@hotmail.com>
2022-03-07 23:50:36 +0300

    Reimplemented as templated class.

--- include/Vortex/Common/Handle.h
+++ include/Vortex/Common/Handle.h
@@ -3,19 +3,23 @@
 namespace Vortex {
 	using HandleType = std::uint32_t;
 	constexpr static HandleType InvalidID = -1;
+
+	template<typename T>
+	struct Handle {
+		using Type = T;
+		Vortex::HandleType id{Vortex::InvalidID};
+
+		Handle(): id{InvalidID} {}
+		Handle(Vortex::HandleType id_): id{id_} {}
+
+		constexpr friend bool operator==(Handle<T> rhs, Handle<T> lhs) { return rhs.id == lhs.id; }
+		constexpr friend bool operator!=(Handle<T> rhs, Handle<T> lhs) { return rhs.id != lhs.id; }
+	};
 }
 
-#define VORTEX_HANDLE(klass)														\
-struct klass {																		\
-Vortex::HandleType id{Vortex::InvalidID};											\
-constexpr friend bool operator==(klass rhs, klass lhs) { return rhs.id == lhs.id;}	\
-constexpr friend bool operator!=(klass rhs, klass lhs) { return rhs.id != lhs.id;}	\
-}
-
-#define VORTEX_HANDLE_HASH(klass)													\
-template<>																			\
-struct std::hash<klass> {															\
-	std::size_t operator()(const klass& s) const noexcept {							\
-		return std::hash<Vortex::HandleType>{}(s.id);								\
-	}																				\
-}																					\
+template<typename T>
+struct std::hash<Vortex::Handle<T>> {
+	std::size_t operator()(const Vortex::Handle<T>& s) const noexcept {
+		return std::hash<Vortex::HandleType>{}(s.id);
+	}
+};
