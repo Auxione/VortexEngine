5877ea983d0e6e6171a9527baba46155d0842ed2
Auxione <crouzer97@hotmail.com>
2022-03-26 11:51:56 +0300

    Merged into Memory.h.

--- include/Vortex/Memory/Memory.h
+++ include/Vortex/Memory/Memory.h
@@ -42,3 +42,21 @@ namespace Vortex {
 namespace Vortex {
 	using String = std::string;
 }
+
+#include <memory>
+namespace Vortex {
+	template<typename T>
+	using Shared = std::shared_ptr<T>;
+
+	template<typename T>
+	using Unique = std::unique_ptr<T>;
+
+	template<typename T>
+	using Weak = std::weak_ptr<T>;
+
+	template<typename T, typename ... Args>
+	inline static Shared<T> MakeShared(Args&& ... args) { return std::make_shared<T>(std::forward<Args>(args)...); }
+
+	template<typename T, typename ... Args>
+	inline static Unique<T> MakeUnique(Args&& ... args) { return std::make_unique<T>(std::forward<Args>(args)...); }
+}
