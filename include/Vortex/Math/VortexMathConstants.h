30946ff59c5790df0a29d2aa49d6c1ac84df7fa2
Auxione <crouzer97@hotmail.com>
2022-03-29 09:54:51 +0300

    Added TWOPI constant.

--- include/Vortex/Math/VortexMathConstants.h
+++ include/Vortex/Math/VortexMathConstants.h
@@ -2,6 +2,7 @@
 
 namespace Vortex::Math {
 	constexpr static float PI = 3.14159265359f;
+	constexpr static float TWOPI = 2.0f * PI;
 	constexpr static float DegToRad = (PI * 2.0f) / 360.0f;
 	constexpr static float RadToDeg = 360.0f / (PI * 2.0f);
 	constexpr static float Epsilon = 0.00001f;
